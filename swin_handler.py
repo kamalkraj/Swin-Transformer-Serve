14bf4d64eca38bf251ae8f911611908e3efebea8
Kamal Raj <kamalraj97@gmail.com>
2021-04-17 16:02:36 +0530

    base

---
+++ swin_handler.py
@@ -0,0 +1,259 @@
+import importlib
+import os
+
+import torch
+import yaml
+from timm.data.constants import IMAGENET_DEFAULT_MEAN, IMAGENET_DEFAULT_STD
+from timm.data.transforms import _pil_interp
+from torchvision import transforms
+from ts.torch_handler.image_classifier import ImageClassifier
+from ts.utils.util import list_classes_from_module
+from yacs.config import CfgNode as CN
+
+_C = CN()
+
+# Base config files
+_C.BASE = ['']
+
+# -----------------------------------------------------------------------------
+# Data settings
+# -----------------------------------------------------------------------------
+_C.DATA = CN()
+# Batch size for a single GPU, could be overwritten by command line argument
+_C.DATA.BATCH_SIZE = 128
+# Path to dataset, could be overwritten by command line argument
+_C.DATA.DATA_PATH = ''
+# Dataset name
+_C.DATA.DATASET = 'imagenet'
+# Input image size
+_C.DATA.IMG_SIZE = 224
+# Interpolation to resize image (random, bilinear, bicubic)
+_C.DATA.INTERPOLATION = 'bicubic'
+# Use zipped dataset instead of folder dataset
+# could be overwritten by command line argument
+_C.DATA.ZIP_MODE = False
+# Cache Data in Memory, could be overwritten by command line argument
+_C.DATA.CACHE_MODE = 'part'
+# Pin CPU memory in DataLoader for more efficient (sometimes) transfer to GPU.
+_C.DATA.PIN_MEMORY = True
+# Number of data loading threads
+_C.DATA.NUM_WORKERS = 8
+
+# -----------------------------------------------------------------------------
+# Model settings
+# -----------------------------------------------------------------------------
+_C.MODEL = CN()
+# Model type
+_C.MODEL.TYPE = 'swin'
+# Model name
+_C.MODEL.NAME = 'swin_tiny_patch4_window7_224'
+# Checkpoint to resume, could be overwritten by command line argument
+_C.MODEL.RESUME = ''
+# Number of classes, overwritten in data preparation
+_C.MODEL.NUM_CLASSES = 1000
+# Dropout rate
+_C.MODEL.DROP_RATE = 0.0
+# Drop path rate
+_C.MODEL.DROP_PATH_RATE = 0.1
+# Label Smoothing
+_C.MODEL.LABEL_SMOOTHING = 0.1
+
+# Swin Transformer parameters
+_C.MODEL.SWIN = CN()
+_C.MODEL.SWIN.PATCH_SIZE = 4
+_C.MODEL.SWIN.IN_CHANS = 3
+_C.MODEL.SWIN.EMBED_DIM = 96
+_C.MODEL.SWIN.DEPTHS = [2, 2, 6, 2]
+_C.MODEL.SWIN.NUM_HEADS = [3, 6, 12, 24]
+_C.MODEL.SWIN.WINDOW_SIZE = 7
+_C.MODEL.SWIN.MLP_RATIO = 4.
+_C.MODEL.SWIN.QKV_BIAS = True
+_C.MODEL.SWIN.QK_SCALE = None
+_C.MODEL.SWIN.APE = False
+_C.MODEL.SWIN.PATCH_NORM = True
+
+# -----------------------------------------------------------------------------
+# Training settings
+# -----------------------------------------------------------------------------
+_C.TRAIN = CN()
+_C.TRAIN.START_EPOCH = 0
+_C.TRAIN.EPOCHS = 300
+_C.TRAIN.WARMUP_EPOCHS = 20
+_C.TRAIN.WEIGHT_DECAY = 0.05
+_C.TRAIN.BASE_LR = 5e-4
+_C.TRAIN.WARMUP_LR = 5e-7
+_C.TRAIN.MIN_LR = 5e-6
+# Clip gradient norm
+_C.TRAIN.CLIP_GRAD = 5.0
+# Auto resume from latest checkpoint
+_C.TRAIN.AUTO_RESUME = True
+# Gradient accumulation steps
+# could be overwritten by command line argument
+_C.TRAIN.ACCUMULATION_STEPS = 0
+# Whether to use gradient checkpointing to save memory
+# could be overwritten by command line argument
+_C.TRAIN.USE_CHECKPOINT = False
+
+# LR scheduler
+_C.TRAIN.LR_SCHEDULER = CN()
+_C.TRAIN.LR_SCHEDULER.NAME = 'cosine'
+# Epoch interval to decay LR, used in StepLRScheduler
+_C.TRAIN.LR_SCHEDULER.DECAY_EPOCHS = 30
+# LR decay rate, used in StepLRScheduler
+_C.TRAIN.LR_SCHEDULER.DECAY_RATE = 0.1
+
+# Optimizer
+_C.TRAIN.OPTIMIZER = CN()
+_C.TRAIN.OPTIMIZER.NAME = 'adamw'
+# Optimizer Epsilon
+_C.TRAIN.OPTIMIZER.EPS = 1e-8
+# Optimizer Betas
+_C.TRAIN.OPTIMIZER.BETAS = (0.9, 0.999)
+# SGD momentum
+_C.TRAIN.OPTIMIZER.MOMENTUM = 0.9
+
+# -----------------------------------------------------------------------------
+# Augmentation settings
+# -----------------------------------------------------------------------------
+_C.AUG = CN()
+# Color jitter factor
+_C.AUG.COLOR_JITTER = 0.4
+# Use AutoAugment policy. "v0" or "original"
+_C.AUG.AUTO_AUGMENT = 'rand-m9-mstd0.5-inc1'
+# Random erase prob
+_C.AUG.REPROB = 0.25
+# Random erase mode
+_C.AUG.REMODE = 'pixel'
+# Random erase count
+_C.AUG.RECOUNT = 1
+# Mixup alpha, mixup enabled if > 0
+_C.AUG.MIXUP = 0.8
+# Cutmix alpha, cutmix enabled if > 0
+_C.AUG.CUTMIX = 1.0
+# Cutmix min/max ratio, overrides alpha and enables cutmix if set
+_C.AUG.CUTMIX_MINMAX = None
+# Probability of performing mixup or cutmix when either/both is enabled
+_C.AUG.MIXUP_PROB = 1.0
+# Probability of switching to cutmix when both mixup and cutmix enabled
+_C.AUG.MIXUP_SWITCH_PROB = 0.5
+# How to apply mixup/cutmix params. Per "batch", "pair", or "elem"
+_C.AUG.MIXUP_MODE = 'batch'
+
+# -----------------------------------------------------------------------------
+# Testing settings
+# -----------------------------------------------------------------------------
+_C.TEST = CN()
+# Whether to use center crop when testing
+_C.TEST.CROP = True
+
+# -----------------------------------------------------------------------------
+# Misc
+# -----------------------------------------------------------------------------
+# Mixed precision opt level, if O0, no amp is used ('O0', 'O1', 'O2')
+# overwritten by command line argument
+_C.AMP_OPT_LEVEL = ''
+# Path to output folder, overwritten by command line argument
+_C.OUTPUT = ''
+# Tag of experiment, overwritten by command line argument
+_C.TAG = 'default'
+# Frequency to save checkpoint
+_C.SAVE_FREQ = 1
+# Frequency to logging info
+_C.PRINT_FREQ = 10
+# Fixed random seed
+_C.SEED = 0
+# Perform evaluation only, overwritten by command line argument
+_C.EVAL_MODE = False
+# Test throughput only, overwritten by command line argument
+_C.THROUGHPUT_MODE = False
+# local rank for DistributedDataParallel, given by command line argument
+_C.LOCAL_RANK = 0
+
+
+def _update_config_from_file(config, cfg_file):
+    config.defrost()
+    with open(cfg_file, 'r') as f:
+        yaml_cfg = yaml.load(f, Loader=yaml.FullLoader)
+
+    for cfg in yaml_cfg.setdefault('BASE', ['']):
+        if cfg:
+            _update_config_from_file(
+                config, os.path.join(os.path.dirname(cfg_file), cfg)
+            )
+    print('=> merge config from {}'.format(cfg_file))
+    config.merge_from_file(cfg_file)
+    config.freeze()
+
+
+def update_config(config: CN, cfg_path):
+    _update_config_from_file(config, cfg_path)
+
+    config.freeze()
+
+
+def get_config(cfg_path):
+    """Get a yacs CfgNode object with default values."""
+    # Return a clone so that the defaults will not be altered
+    # This is for the "local variable" use pattern
+    config = _C.clone()
+    update_config(config, cfg_path)
+
+    return config
+
+
+class SwinClassifier(ImageClassifier):
+    """
+    SwinClassifier handler class.
+    """
+    image_processing = None
+
+    def _load_pickled_model(self, model_dir, model_file, model_pt_path):
+        self.model_config = get_config(os.path.join(model_dir, "swin_config.yaml"))
+        resize_im = self.model_config.DATA.IMG_SIZE > 32
+        t = []
+        if resize_im:
+            if self.model_config.TEST.CROP:
+                size = int((256 / 224) * self.model_config.DATA.IMG_SIZE)
+                t.append(
+                    transforms.Resize(size, interpolation=_pil_interp(self.model_config.DATA.INTERPOLATION)),
+                    # to maintain same ratio w.r.t. 224 images
+                )
+                t.append(transforms.CenterCrop(self.model_config.DATA.IMG_SIZE))
+            else:
+                t.append(
+                    transforms.Resize((self.model_config.DATA.IMG_SIZE, self.model_config.DATA.IMG_SIZE),
+                                    interpolation=_pil_interp(self.model_config.DATA.INTERPOLATION))
+                )
+
+        t.append(transforms.ToTensor())
+        t.append(transforms.Normalize(IMAGENET_DEFAULT_MEAN, IMAGENET_DEFAULT_STD))
+        SwinClassifier.image_processing = transforms.Compose(t)
+        model_def_path = os.path.join(model_dir, model_file)
+        if not os.path.isfile(model_def_path):
+            raise RuntimeError("Missing the model.py file")
+
+        module = importlib.import_module(model_file.split(".")[0])
+        model_class_definitions = list_classes_from_module(module)
+
+        model_class_definitions = {str(i):i for i in model_class_definitions}
+
+        model_class = model_class_definitions["<class 'swin_transformer.SwinTransformer'>"]
+        state_dict = torch.load(model_pt_path, map_location=self.map_location)["model"]
+        model = model_class(img_size=self.model_config.DATA.IMG_SIZE,
+                                patch_size=self.model_config.MODEL.SWIN.PATCH_SIZE,
+                                in_chans=self.model_config.MODEL.SWIN.IN_CHANS,
+                                num_classes=self.model_config.MODEL.NUM_CLASSES,
+                                embed_dim=self.model_config.MODEL.SWIN.EMBED_DIM,
+                                depths=self.model_config.MODEL.SWIN.DEPTHS,
+                                num_heads=self.model_config.MODEL.SWIN.NUM_HEADS,
+                                window_size=self.model_config.MODEL.SWIN.WINDOW_SIZE,
+                                mlp_ratio=self.model_config.MODEL.SWIN.MLP_RATIO,
+                                qkv_bias=self.model_config.MODEL.SWIN.QKV_BIAS,
+                                qk_scale=self.model_config.MODEL.SWIN.QK_SCALE,
+                                drop_rate=self.model_config.MODEL.DROP_RATE,
+                                drop_path_rate=self.model_config.MODEL.DROP_PATH_RATE,
+                                ape=self.model_config.MODEL.SWIN.APE,
+                                patch_norm=self.model_config.MODEL.SWIN.PATCH_NORM,
+                                use_checkpoint=self.model_config.TRAIN.USE_CHECKPOINT)
+        model.load_state_dict(state_dict)
+        return model
---
+++ weights/README.md
@@ -0,0 +1,3 @@
+# Download
+
+Download pre-trained model weights from official [repo](https://github.com/microsoft/Swin-Transformer#main-results-on-imagenet-with-pretrained-models) and place in this folder.
